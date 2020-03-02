6db14dd674c215ca3bb3b9b0febfb513a0d8be6c
Aarav Chandra <aarav@Aaravs-MBP.attlocal.net>
2020-03-02 02:26:57 -0500

    initial

---
+++ Remove Vowels from a String.py
@@ -0,0 +1,26 @@
+# Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+
+
+# Example 1:
+
+# Input: "leetcodeisacommunityforcoders"
+# Output: "ltcdscmmntyfrcdrs"
+# Example 2:
+
+# Input: "aeiou"
+# Output: ""
+
+
+# Note:
+
+# S consists of lowercase English letters only.
+# 1 <= S.length <= 1000
+
+class Solution:
+    def removeVowels(self, S: str) -> str:
+        l = ['a', 'e', 'i', 'o', 'u']
+        s = ""
+        for i in S:
+            if (i not in l):
+                s = s+i
+        return s
